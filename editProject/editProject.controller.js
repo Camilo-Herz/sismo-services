a11c0ac8c9458d3052e5cd835edbcd198b8f41da
Camilo Hernandez <juancas.1420@gmail.com>
2021-04-10 22:20:01 -0500

    Ajuste edicion de URI opc

--- editProject/editProject.controller.js
+++ editProject/editProject.controller.js
@@ -13,9 +13,9 @@ var controller = {
         const id = req.params.id;
         let user = await User.findById(id);
         if (req.body.deleteProject) {
-            const projectRemove = user.projects.find(project => project.idProject === req.body.idProject);
-            if (projectRemove) {
-                user.projects.pull(projectRemove);
+            const project = user.projects.find(project => project.idProject === req.body.idProject);
+            if (project) {
+                user.projects.pull(project);
                 await user.save((err, data) => {
                     if (err) return res.status(404).send({
                         status: 2,
@@ -33,6 +33,29 @@ var controller = {
                     });
                 });
             }
+        } else if (req.body.editEndpointOPC) {
+            await User.updateOne({ 'projects.idProject': req.body.idProject }, {
+                '$set': {
+                    'projects.$.endpointOPC': req.body.newEndpoint
+                }
+            }, (err) => {
+                if (err) return res.status(404).send({
+                    status: 2,
+                    message: 'No fue posible crear un nuevo proyecto',
+                    labelBtnDerecha: 'Aceptar',
+                    stepId: 'connections'
+                });
+                const indexProject = user.projects.findIndex((element) => element.idProject === req.body.idProject);
+                user.projects[indexProject].endpointOPC = req.body.newEndpoint;
+                controllerSession.setDataSession('projects', user.projects);
+                return res.status(200).send({
+                    status: 1,
+                    stepId: 'connections',
+                    payload: {
+                        projects: user.projects
+                    }
+                });
+            });
         } else {
             user.projects.push(req.body);
             await user.save((err, data) => {
