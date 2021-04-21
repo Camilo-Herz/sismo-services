c15626b87bae60880fe9bd32f12ae5bbe44b8879
Camilo Hernandez <juancas.1420@gmail.com>
2021-04-21 18:35:12 -0500

    Ajuste entorno

--- config/properties.js
+++ config/properties.js
@@ -1,5 +1,5 @@
 module.exports = {
-    PORT: process.env.PORT,
+    PORT: process.env.PORT || 3700,
     DB: 'mongodb+srv://user-opcua:Camilo1234@conectopcua.h7wtq.mongodb.net/mydatabase?retryWrites=true&w=majority',
     OPC_SERVER: "opc.tcp://localhost:4840",
     TAG_OPC: "ns=4;s=|var|CODESYS Control Win V3 x64.Application.PLC_PRG."
