4e7d3b969242c93e4e7d4319f40ee14993c667ab
Camilo Hernandez <juancas.1420@gmail.com>
2021-05-06 00:28:14 -0500

    mail

--- routes/app.routes.js
+++ routes/app.routes.js
@@ -3,6 +3,7 @@ const createProject = require('../editProject/editProject.controller');
 const process = require('../processes/processes.controller');
 const navegate = require('../navegate/navegate.controller');
 const localServices = require('../localService/localService.controller');
+const mail = require('../mailer/mail.controller');
 
 module.exports = (router) => {
     router.post('/register', Users.createUser);
@@ -12,4 +13,5 @@ module.exports = (router) => {
     router.put('/project/:id', createProject.editProject);
     router.get('/pageNavigation/:key/:id', navegate.pageNavigation);
     router.post('/localService', localServices.localService);
+    router.post('/mail', mail.sendAlertMail);
 }
