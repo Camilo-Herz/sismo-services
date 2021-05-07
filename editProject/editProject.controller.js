331b0ace3ee83fa1e4cb8c46ef3eb47aea52c803
Camilo Hernandez <juancas.1420@gmail.com>
2021-05-06 20:50:25 -0500

    conexiones

--- editProject/editProject.controller.js
+++ editProject/editProject.controller.js
@@ -3,7 +3,7 @@
 const User = require('../auth/auth.dao');
 const modelNewProject = require('./editProject.model');
 const controllerSession = require('../navegate/navegate.controller');
-
+const messagesStore = require('../config/messageStore');
 
 // Respuestas http
 // https://developer.mozilla.org/es/docs/Web/HTTP/Status
@@ -13,81 +13,25 @@ var controller = {
         const id = req.params.id;
         let user = await User.findById(id);
         if (req.body.deleteProject) {
-            const project = user.projects.find(project => project.idProject === req.body.idProject);
-            if (project) {
-                user.projects.pull(project);
-                await user.save((err, data) => {
-                    if (err) return res.status(404).send({
-                        status: 2,
-                        message: 'No fue posible eliminar el proyecto',
-                        labelBtnDerecha: 'Aceptar',
-                        stepId: 'dashboard'
-                    });
-                    controllerSession.editDataSession(user._id, user.projects);
-                    return res.status(200).send({
-                        status: 1,
-                        stepId: 'dashboard',
-                        payload: {
-                            projects: user.projects
-                        }
-                    });
-                });
-            }
+            deleteProject(user, req, res);
         } else if (req.body.editEndpointOPC) {
-            await User.updateOne({ 'projects.idProject': req.body.idProject }, {
-                '$set': {
-                    'projects.$.endpointOPC': req.body.newEndpoint
-                }
-            }, (err) => {
-                if (err) return res.status(404).send({
-                    status: 2,
-                    message: 'No fue posible crear un nuevo proyecto',
-                    labelBtnDerecha: 'Aceptar',
-                    stepId: 'connections'
-                });
-                const indexProject = user.projects.findIndex((element) => element.idProject === req.body.idProject);
-                user.projects[indexProject].endpointOPC = req.body.newEndpoint;
-                controllerSession.editDataSession(user._id, user.projects);
-                return res.status(200).send({
-                    status: 1,
-                    stepId: 'connections',
-                    payload: {
-                        projects: user.projects
-                    }
-                });
-            });
-        } else if (req.body.newTopics) {
-            await User.updateOne({ 'projects.idProject': req.body.idProject }, {
-                '$set': {
-                    'projects.$.topics': req.body.newTopics
-                }
-            }, (err) => {
-                if (err) return res.status(404).send({
-                    status: 2,
-                    message: 'No fue posible editar el topic',
-                    labelBtnDerecha: 'Aceptar',
-                    stepId: 'connections'
-                });
-                const indexProject = user.projects.findIndex((element) => element.idProject === req.body.idProject);
-                user.projects[indexProject].topics = req.body.newTopics;
-                controllerSession.editDataSession(user._id, user.projects);
-                return res.status(200).send({
-                    status: 1,
-                    stepId: 'connections',
-                    payload: {
-                        projects: user.projects
-                    }
-                });
-            });
+            editEndpointOPC(user, req, res);
+        } else if (req.body.editTopic) {
+            editTopic(user, req, res);
         } else {
+            const valuesTopicsDefaults = [];
+            req.body.topics.forEach(element => {
+                valuesTopicsDefaults.push({
+                    name: element,
+                    alert: false,
+                    valueAlert: 'NA',
+                    units: 'NA'
+                });
+            });
+            req.body.topics = valuesTopicsDefaults;
             user.projects.push(req.body);
             await user.save((err, data) => {
-                if (err) return res.status(404).send({
-                    status: 2,
-                    message: 'No fue posible crear un nuevo proyecto',
-                    labelBtnDerecha: 'Aceptar',
-                    stepId: 'dashboard'
-                });
+                if (err) return res.status(404).send(messagesStore.ERR_NEW_PROJECT);
                 controllerSession.editDataSession(user._id, user.projects);
                 return res.status(200).send({
                     status: 1,
@@ -98,6 +42,64 @@ var controller = {
                 });
             });
         }
+    }
+}
+
+const editTopic = async (user, req, res) => {
+    await User.updateOne({ 'projects.idProject': req.body.idProject }, {
+        '$set': {
+            'projects.$.topics': req.body.newTopics
+        }
+    }, (err) => {
+        if (err) return res.status(404).send(messagesStore.ERR_EDIT_TOPIC);
+        const indexProject = user.projects.findIndex((element) => element.idProject === req.body.idProject);
+        user.projects[indexProject].topics = req.body.newTopics;
+        controllerSession.editDataSession(user._id, user.projects);
+        return res.status(200).send({
+            status: 1,
+            stepId: 'connections',
+            payload: {
+                projects: user.projects
+            }
+        });
+    });
+}
+
+const editEndpointOPC = async (user, req, res) => {
+    await User.updateOne({ 'projects.idProject': req.body.idProject }, {
+        '$set': {
+            'projects.$.endpointOPC': req.body.newEndpoint
+        }
+    }, (err) => {
+        if (err) return res.status(404).send(messagesStore.ERR_EDIT_ENDPOINT_OPC);
+        const indexProject = user.projects.findIndex((element) => element.idProject === req.body.idProject);
+        user.projects[indexProject].endpointOPC = req.body.newEndpoint;
+        controllerSession.editDataSession(user._id, user.projects);
+        return res.status(200).send({
+            status: 1,
+            stepId: 'connections',
+            payload: {
+                projects: user.projects
+            }
+        });
+    });
+}
+
+const deleteProject = async (user, req, res) => {
+    const project = user.projects.find(project => project.idProject === req.body.idProject);
+    if (project) {
+        user.projects.pull(project);
+        await user.save((err, data) => {
+            if (err) return res.status(404).send(messagesStore.ERR_DELETE_PROJECT);
+            controllerSession.editDataSession(user._id, user.projects);
+            return res.status(200).send({
+                status: 1,
+                stepId: 'dashboard',
+                payload: {
+                    projects: user.projects
+                }
+            });
+        });
     }
 }
 
