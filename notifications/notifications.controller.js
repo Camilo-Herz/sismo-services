72ea665b2585f65ec906752a154159008110c2cb
Camilo Hernandez <juancas.1420@gmail.com>
2021-09-16 05:22:40 -0500

    Ajuste alertas

--- notifications/notifications.controller.js
+++ notifications/notifications.controller.js
@@ -1,9 +1,27 @@
 
+const Alerts = require("./notifications.dao");
+
 exports.getNotifications = (req, res) => {
-    return res.status(203).send({
-        stepId: 'notifications',
-        payload: {
-            alerts: []
+    const userData = {
+        user: req.body.userId,
+    };
+    Alerts.find({ userId: userData.user }, (err, user) => {
+        if (err) return res.status(500).send("Error de servidor");
+        if (!user) {
+            return res.status(203).send({
+                status: 2,
+                message: "No se ha podido acceder a la ruta",
+                labelBtnDerecha: "Aceptar",
+                stepId: "login",
+            });
+        } else {
+            return res.send({
+                status: 1,
+                stepId: 'notifications',
+                payload: {
+                    alerts: user
+                },
+            });
         }
     });
 };
